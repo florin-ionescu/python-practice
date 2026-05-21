08921cbeb7911ddf8d7fc7e82c33894ea4a588b4
Florin Ionescu <126186642+Cichicean@users.noreply.github.com>
2026-05-21 20:58:52 +0300

    Practiced while loops

--- control_flow/while_loops.py
+++ control_flow/while_loops.py
@@ -1,1 +1,32 @@
-#work in progress
+# WHILE Loops
+
+"""
+A while loop repeats code as long as a condition is true.
+
+while condition:
+    # code to repeat
+
+"""
+
+count = 1
+
+while count <= 5:
+    print(count)
+    count += 1
+
+
+password = ""
+
+while password != "python123":
+    password = input("Enter password: ")
+
+print("Access granted")
+
+
+number = 5
+
+while number > 0:
+    print(number)
+    number -= 1
+
+print("Go!")
