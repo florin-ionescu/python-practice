f1de13b58550c2cd7e8d73cecbcfea14fd170520
Florin Ionescu <126186642+Cichicean@users.noreply.github.com>
2026-05-21 20:48:07 +0300

    Practiced for loops

--- control_flow/for_loops.py
+++ control_flow/for_loops.py
@@ -1,4 +1,12 @@
-#for loops
+#for
+
+'''
+- A for loop is used to repeat a block of code a known number of times,
+or to go through items in a collection such as a list, string, range, array, dictionary, or file.
+
+for item in collection:
+    do_something(item)
+'''
 
 grades = [90, 85, 78, 92, 88]
 total = 0
@@ -16,3 +24,16 @@ print(total2 / amount)
 #range(5) va genera numere de la 0 la 4
 for i in range(5):
     print(i)
+
+
+word = "banana"
+count = 0
+for letter in word:
+    if letter == "a":
+        count += 1
+print(f'found {count} letters a') #put print outside loop to print only the final total
+
+c = 0
+c = word.count("a")
+print(c)
+
