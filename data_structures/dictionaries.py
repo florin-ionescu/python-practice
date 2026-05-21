97af86de6c7f8325509ba1e150aa97d07f107507
Florin Ionescu <126186642+Cichicean@users.noreply.github.com>
2026-05-21 17:26:33 +0300

    Worked on dictionaries.py

--- data_structures/dictionaries.py
+++ data_structures/dictionaries.py
@@ -4,14 +4,25 @@
 this_is_a_dictionary = {"key" : "value", "key2" : "value2"}
 
 
-my_dictionary = {"name" : "Florin", "age" : 37, "height" : 1.83, "is_student" : True}
+my_dictionary = {
+    "name" : "Florin",
+    "age" : 37,
+    "height" : 1.83,
+    "is_student" : True
+}
 
 #accessing value by key
 print(my_dictionary["name"])
 print(this_is_a_dictionary["key"])
 
+my_dictionary["sport"] = "Aikido"
+print(my_dictionary)
+
 for key in my_dictionary:
     # prints the keys of the dictionary
     print(key)
 
+print(my_dictionary["sport"])
+print(my_dictionary.get("name"))
 
+# Dictionaries are great when you want to look something up by name, ID, label, or category.
