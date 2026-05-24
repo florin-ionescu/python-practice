e45e1e21e6df2d9535e4cde5d8891fbe9d8b658c
Florin Ionescu <126186642+Cichicean@users.noreply.github.com>
2026-05-24 09:02:29 +0300

    Practiced nested conditions

--- control_flow/nested_conditions.py
+++ control_flow/nested_conditions.py
@@ -1,1 +1,14 @@
-#work in progress
+# Nested conditions means putting an if statement inside another if statement.
+
+age = 18
+has_id = False
+
+if age >= 18:
+    # So the inner condition only runs if the outer condition is true.
+    if has_id:
+        print("You are eligible to vote!")
+    else:
+        print("You are not eligible to vote!")
+else:
+    print("You are too young to vote!")
+
