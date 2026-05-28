8f6a4fb74534f40b273afbb0e32ccc122b0d5c51
Florin Ionescu <126186642+Cichicean@users.noreply.github.com>
2026-05-28 18:55:33 +0300

    Recursion updated

--- advanced/recursion.py
+++ advanced/recursion.py
@@ -1,1 +1,19 @@
-# Work in progress
+'''
+RECURSION refers to a function calling itself to solve a problem.
+It involves two critical components:
+Base case: This is the condition that terminates the recursion.
+
+Without it, the recursive calls would continue forever, eventually causing the function to crash or exhaust available memory
+'''
+
+
+# example
+def countdown(n):
+    print(n)
+
+    if n == 0:
+        return
+
+    countdown(n - 1)
+
+countdown(3)
