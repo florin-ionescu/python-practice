7a8c268c7bead1079b0c4e08832f14110d26430b
Florin Ionescu <126186642+Cichicean@users.noreply.github.com>
2026-05-30 14:51:48 +0300

    Worked on enumerate

--- built_in_functions/enumerate.py
+++ built_in_functions/enumerate.py
@@ -5,6 +5,13 @@ names = ['Florin', 'Cristina', "Smiorchi", "Inky"]
 for index, name in enumerate(names, start=1):
     print(index, name)
 
-#
 
+countries = ["Romania", "Italy", "France", "Germany"]
+for index in range(len(countries)):
+    print(f"{index+1} - {countries[index]}")
 
+for item in enumerate(countries, start=1):
+    print(item)
+
+for index, country in enumerate(countries, start=1):
+    print(f"{index}. {country}")
