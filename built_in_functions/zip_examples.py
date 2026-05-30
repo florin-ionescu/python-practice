2100aee307550729d706c9be4743de9634c46bd8
Florin Ionescu <126186642+Cichicean@users.noreply.github.com>
2026-05-30 14:52:05 +0300

    Worked on zip and zip_longest (itertools).

--- built_in_functions/zip_examples.py
+++ built_in_functions/zip_examples.py
@@ -1,3 +1,5 @@
+from itertools import zip_longest
+
 # zip() - Useful for combining lists:
 
 names = ['Florin', 'Cristina', "Smiorchi", "Inky"]
@@ -8,3 +10,13 @@ for name, score in zip(names, scores):
 
 # enumerate() → number menu options in CLI app
 # zip() → combine names + scores / columns + values
+
+countries = ["Romania", "Italy", "France", "Germany", "Hungary", "Japan", "USA"]
+capitals = ["Bucharest", "Rome", "Paris", "Berlin"]
+
+for country, capital in zip(countries, capitals):
+    print(f'The capital of {country} is {capital}')
+
+
+for country, capital in zip_longest(countries, capitals, fillvalue="Unknown"):
+    print(f'The capital of {country} is {capital}')
